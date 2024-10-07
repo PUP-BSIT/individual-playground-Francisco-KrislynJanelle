e1793f5ec27241f2bda6afb1f47d42901f11b076
Krislyn Janelle Francisco <krislynfrancisco0815@gmail.com>
2024-10-07 19:46:41 +0800

    my info

---
+++ Francisco_Krislyn.py
@@ -0,0 +1,8 @@
+Name = "Krislyn Janelle Francisco"
+Birthday = "\nAugust 20,2005"
+Gender = "\nFemale"
+Email = "\nkrislynfrancisco0815@gmail.com"
+
+print (Name, Birthday, Gender, Email)
+dm = "Welcome to my file!"
+print (dm)
